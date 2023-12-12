e387097497ee28f9d334b4b34c51036a64d01f11
sonu <srssoumyaranjansahu123@gmail.com>
2023-12-12 15:03:59 +0530

    backend(order)

--- Backend/routes/orderRoutes.js
+++ Backend/routes/orderRoutes.js
@@ -1,6 +1,6 @@
 
 const express = require('express');
-const { newOrder, singleOrder, allorders, deleteAOrder, updateAOrder } = require('../controller/orderController');
+const { newOrder, singleOrder, allorders, deleteAOrder, updateAOrder ,allordersAdmin} = require('../controller/orderController');
 const { isAuthenticate, isAuthorise } = require('../middlewear/tokenAuth');
 
 const router = express.Router();
@@ -8,6 +8,7 @@ const router = express.Router();
 router.post('/newOrder', isAuthenticate, newOrder);
 router.get('/orders/singleOrder/:id', isAuthenticate, isAuthorise("admin"), singleOrder);
 router.get('/userallorders', isAuthenticate, allorders);
+router.get('/admin/userallorders', isAuthenticate,isAuthorise("admin"), allordersAdmin);
 router.delete('/deleteorder/:orderId', isAuthenticate, isAuthorise("admin"), deleteAOrder);
 //adminupdate
 router.put('/admin/updateOrder/:orderId', isAuthenticate, isAuthorise("admin"), updateAOrder);
