d0e6b2498aa9201e55d4ff8cec4c21add020906c
sonu <srssoumyaranjansahu123@gmail.com>
2023-12-15 18:04:33 +0530

    frontend

--- frontend/src/Component/Ecommerce/EcommerceHome.jsx
+++ frontend/src/Component/Ecommerce/EcommerceHome.jsx
@@ -1,16 +1,31 @@
 import React from 'react'
 import { Header } from './Header'
+import Carousel from './Carousel'
+import { Category } from './Category'
+import { Loaderproduct } from './Loaderproduct'
+import { Footer } from './Footer'
 
 function EcommerceHome() {
   return (
-   
-      <React.Fragment >
-        <div className=' h-3/4 w-full '>
-    <Header/>
-        </div>
-        
-      </React.Fragment>
-   
+
+
+    <div className='  h-full w-full flex flex-col '>
+      <Header />
+
+      <Carousel />
+
+
+      <Category />
+      <Carousel />
+
+      <Category />
+      <Footer />
+
+
+    </div>
+
+
+
   )
 }
 
