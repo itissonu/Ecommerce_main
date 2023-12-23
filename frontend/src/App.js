f0119ab4d4ef9f1c04a95090fa4b8c79b072ff15
Sumanta <sksahu2022@gift.edu.in>
2023-12-23 22:44:44 +0530

    single product

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,16 @@ import Product from './Component/Admin/Product';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import EcommerceHome from './Component/Ecommerce/EcommerceHome';
+import SingleProduct from './Component/Ecommerce/SingleProduct';
+import { Productspage } from './Component/Ecommerce/product/Productspage';
+import { Cart } from './Component/Ecommerce/cart/Cart';
+import { AddAddress } from './Component/Ecommerce/cart/AddAddress';
+import  Payment  from './Component/Ecommerce/cart/Payment';
+import { MyOrders } from './pages/MyOrders';
+import { ForgotPassword } from './pages/ForgotPassword';
+import { ResetPassword } from './pages/ResetPassword';
+import { MyProfile } from './pages/MyProfile';
+import { Wishlist } from './Component/Ecommerce/cart/Wishlist';
 function App() {
   return (
     <Router>
@@ -17,7 +27,16 @@ function App() {
         <Route path='/password/forgot' element={<ForgotPassword/>} />
         <Route path='/password/reset/:token' element={<ResetPassword/>} />
         <Route path='/register' element={<Register/>} />
-        <Route path="/productadmin" element={<Product/>} />
+    
+        <Route path='/singleProduct' element ={<SingleProduct />} />
+        <Route path="/admin/productadmin" element={<Product/>} />
+        <Route path='/user/products' element={<Productspage/>}/>
+        <Route path='/user/cart' element={<Cart/>} />
+        <Route path='/user/wishlist' element={<Wishlist/>} />
+        <Route path='/user/deliver-address' element={<AddAddress/>} />
+        <Route path='/user/payment' element={<Payment/>} />
+        <Route path='/user/myorders' element={<MyOrders/>} />
+        <Route path='/user/myprofile' element={<MyProfile/>} />
       </Routes>
     </Router>
   );
